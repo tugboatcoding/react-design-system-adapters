5994c47e35b86dc45b5e90d2820f1ff0bc962055
Mario Hayashi <>
2022-01-26 09:17:49 +0000

    fix: Fix missing param in mapToProps

--- packages/react-potion/src/components/index.js
+++ packages/react-potion/src/components/index.js
@@ -299,7 +299,7 @@ export const PotionComponent = ({ _type, renderer = (v) => v, ...props }) => {
   } else if (_type === 'toggle') {
     newProps = {
       ...props,
-      label: <PotionComponent renderer={renderer} {...mapToProps(props.label)} />,
+      label: <PotionComponent renderer={renderer} {...mapToProps(_type, props.label)} />,
       children: Array.isArray(props.children)
         ? props.children.map((child, idx) => (
           <PotionComponent key={idx} renderer={renderer} {...child} />
